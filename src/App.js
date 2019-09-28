c008f3844571d31653e121f9c17845ce26082305
kseniadumpling <ksenia.dumpling@gmail.com>
2019-09-29 00:03:40 +0300

    Deploy features & small fixes

--- src/App.js
+++ src/App.js
@@ -22,9 +22,9 @@ class App extends Component {
         <Row className="justify-content-md-center pt-5">
           <Col md="auto">
             <h1 className="pt-4 pb-2 text-center">Бинго!</h1>
-            <h5 className="pb-5 ml-5">Собравший всю строку/столбец/диагональ обязан выкрикнуть "бинго" на всю аудиторию<br/>
+            <p className="pb-5 ml-5">Собравший всю строку/столбец/диагональ обязан выкрикнуть "бинго"<br/>
               <i>и пусть Глебовский припомнит вам это на экзамене</i>
-            </h5>
+            </p>
             <BingoTable/>
           </Col>
         </Row>
