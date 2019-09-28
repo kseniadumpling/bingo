69b2d31bbc1bdc6c093e17573e43cc489f3f231e
kseniadumpling <ksenia.dumpling@gmail.com>
2019-09-28 23:58:59 +0300

    Updates

---
+++ precache-manifest.851f5ea507b22de21d7600a682afcd19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e602a019c6836801f54f91cff4333d10",
+    "url": "/bingo/index.html"
+  },
+  {
+    "revision": "aabffbdccfa8d957874c",
+    "url": "/bingo/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "757f66f84d19afbd01c5",
+    "url": "/bingo/static/css/main.c8ef055e.chunk.css"
+  },
+  {
+    "revision": "aabffbdccfa8d957874c",
+    "url": "/bingo/static/js/2.c9ce8af9.chunk.js"
+  },
+  {
+    "revision": "757f66f84d19afbd01c5",
+    "url": "/bingo/static/js/main.b3506999.chunk.js"
+  },
+  {
+    "revision": "c9c4e81425cfa3ecb019",
+    "url": "/bingo/static/js/runtime-main.0fc3e100.js"
+  }
+]);
